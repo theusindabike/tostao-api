583995bde126ee87902cd88c9040646812f2be66
Matheus Lopes <mlopes.matheus@gmail.com>
2020-07-19 22:56:59 -0300

    list person only when authenticat3ed

--- api/persons/views.py
+++ api/persons/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
 
 from api.persons.models import Person, PersonSerializer
@@ -7,3 +8,4 @@ from api.persons.models import Person, PersonSerializer
 class Persons(generics.ListAPIView):
     queryset = Person.objects.all()
     serializer_class = PersonSerializer
+    permission_classes = [IsAuthenticated]
